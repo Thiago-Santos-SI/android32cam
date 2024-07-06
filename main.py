59e74e76a2a33fe3d57b331cbd5d8934a2267f20
thisames <44899195+thisames@users.noreply.github.com>
2024-07-05 21:50:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 import cv2
 import socket
 import struct
-
-# Configuração do endereço IP e porta do servidor
 SERVER_IP = '127.0.0.1'
 SERVER_PORT = 5000
 
-# Criação do socket UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Inicializa a captura da webcam (0 geralmente se refere à webcam padrão)
 cap = cv2.VideoCapture(0)
 
-# Configura a resolução da webcam (opcional, dependendo da sua webcam)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
@@ -24,10 +19,8 @@ while True:
         print("Não foi possível capturar o frame.")
         break
 
-    # Codificação do frame para JPEG
     _, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 85])
 
-    # Prepara e envia o frame
     header = struct.pack('I', frame_id)
     message = header + buffer.tobytes()
 
@@ -35,11 +28,9 @@ while True:
         sock.sendto(message, (SERVER_IP, SERVER_PORT))
     except OSError as e:
         print(f"Erro ao enviar frame: {e}")
-        # Você pode adicionar aqui um controle de fluxo ou uma pausa se necessário
 
     frame_id += 1
 
-    # Pressione 'q' para sair
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
